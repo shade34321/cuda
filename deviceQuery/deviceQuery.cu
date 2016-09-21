188d2a209101e5be90564855035f0fd8c1a29a70
Shade Alabsa <shade34321@gmail.com>
2016-09-21 01:24:06 -0400

    The while loop doesn't work but the for loop does...idk.

--- deviceQuery/deviceQuery.cu
+++ deviceQuery/deviceQuery.cu
@@ -7,20 +7,21 @@ void printProperties(int i, cudaDeviceProp *prop){
         printf( "Name:  %s\n", prop->name );
 }
 
-int main(void){
-	int count;
+int main( void ) {
 	cudaDeviceProp  *prop;
+	int count;
 
 	//Doesn't handle errors
 	cudaGetDeviceCount(&count);
-	
+
 	printf("Device Count: %d\n", count);
 
-	while(count > 0){
+	//while(count > 0){
+	for(int i = 0; i < count; i++){
 		//Doesn't handle errors
-		cudaGetDeviceProperties( prop, count );
-		printProperties(count, prop);	
-		count--;		
+		cudaGetDeviceProperties( prop, i );
+		printProperties(i, prop);	
+		//count--;		
 	}
 
 	return 0;
